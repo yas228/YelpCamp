c4db3131033e98c3de07b4613c7a827ac1c751b5
Yawo Siatitse <asiatits@u.rochester.edu>
2019-02-07 02:14:29 -0500

    AUth

--- app.js
+++ app.js
@@ -1,13 +1,146 @@
-var express = require("express");
-var app = express();
+var express = require("express"),
+    app = express(),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose"),
+    passport = require("passport"),
+    LocalStrategy = require("passport-local"),
+    User = require("./models/user"),
+    Campground = require("./models/campground"),
+    Comment = require("./models/comment");
+    //seedDB = require("./seeds");
 
 
 
+mongoose.connect("mongodb://localhost/yelp_camp", { useNewUrlParser: true } );
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname+"/public"));
+app.set("view engine", "ejs");
+//seedDB();
+app.use(require("express-session")({
+    secret: "This is testing",
+    resave : false,
+    saveUninitialized : false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+app.get("/", function(req, res){
+    res.render("landing");
+});
+
+//show all campgrounds
+app.get("/campgrounds", function(req, res){
+    //Get all campgrounds from DB
+    Campground.find({}, function(err, allcampgrounds){
+        if(err){
+            console.log(err);
+        } else {
+            res.render("campgrounds/index", {campgrounds: allcampgrounds});
+        }
+    });
+});
+
+//create new campgrounds
+app.post("/campgrounds", function(req, res){
+
+    var name = req.body.name; 
+    var image = req.body.image;
+    var desc = req.body.description;
+    var newCampground = {name: name, image: image, description: desc};
+
+    //Create a new campground and save to DB
+    Campground.create(newCampground, function(err, newlyCreated){
+        if(err){
+            console.log(err);
+        } else {
+            res.redirect("/campgrounds");
+        }
+    })
+
+});
+
+//show form to create new campground
+app.get("/campgrounds/new", function(req, res){
+    res.render("campgrounds/new");
+});
+
+
+//shows more info about one campground
+app.get("/campgrounds/:id", function(req, res){
+    //find campground with provided ID
+    Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground){
+        if(err){
+            console.log(err);
+        } else {
+            //render show template with that campground
+            res.render("campgrounds/show", {camp: foundCampground});
+        }
+    });
+});
 
 
 
+//form for new comment
+app.get("/campgrounds/:id/comments/new", function(req, res){
+    //find campground by id
+    Campground.findById(req.params.id, function(err, campground){
+        if(err){
+            console.log(err);
+        } else {
+            res.render("comments/new", {camp: campground});
+        }
+    });
+});
 
 
-app.listen(3000 , function(){
-    console.log("Server listening on Port 3000");
+//create new comment
+app.post("/campgrounds/:id/comments", function(req, res){
+    //lookup campground using ID
+    Campground.findById(req.params.id, function(err, campground){
+        if(err){
+            console.log(err);
+            res.redirect("/campgrounds");
+        } else {
+            //create new comment
+            Comment.create(req.body.comment, function(err, comment){
+                if(err){
+                    console.log(err);
+                } else {
+                    //connect new comment to campground
+                    campground.comments.push(comment);
+                    campground.save();
+                    res.redirect("/campgrounds/" + campground._id);
+                }
+            });
+        }
+    });
+});
+
+//AUTH ROUTES
+app.get("/register", function(req, res) {
+    res.render("register");
+});
+
+app.post("/register", function(req, res) {
+    User.register(new User({username: req.body.username}), req.body.password, function(err, user) {
+        if (err){
+            console.log(err);
+            return res.render("register");
+        } else {
+            passport.authenticate("local")(req, res, function() {
+                res.redirect("/campgrounds");
+            });
+        }
+    });
+});
+
+
+
+app.listen("3000", function(){
+    console.log("YelpCamp running on port 3000");
 });
