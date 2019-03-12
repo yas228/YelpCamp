4ad585fbfdfd5da80c9191c5260f085a2a18a41b
Yawo Siatitse <asiatits@u.rochester.edu>
2019-03-12 00:02:42 -0400

    Refactoring landing and adding midlware

--- app.js
+++ app.js
@@ -1,146 +1,55 @@
-var express = require("express"),
-    app = express(),
-    bodyParser = require("body-parser"),
-    mongoose = require("mongoose"),
-    passport = require("passport"),
+var express     = require("express"),
+    app         = express(),
+    bodyParser  = require("body-parser"),
+    mongoose    = require("mongoose"),
+    flash       = require("connect-flash"),
+    passport    = require("passport"),
     LocalStrategy = require("passport-local"),
-    User = require("./models/user"),
-    Campground = require("./models/campground"),
-    Comment = require("./models/comment");
-    //seedDB = require("./seeds");
+    methodOverride = require("method-override"),
+    Campground  = require("./models/campground"),
+    Comment     = require("./models/comment"),
+    User        = require("./models/user"),
+    seedDB      = require("./seeds")
+    
+//requiring routes
+var commentRoutes    = require("./routes/comments"),
+    campgroundRoutes = require("./routes/campgrounds"),
+    indexRoutes      = require("./routes/index")
+ 
+var url = "mongodb://localhost/yelp_camp";
+mongoose.connect(url);
 
-
-
-mongoose.connect("mongodb://localhost/yelp_camp", { useNewUrlParser: true } );
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static(__dirname+"/public"));
 app.set("view engine", "ejs");
-//seedDB();
-app.use(require("express-session")({
-    secret: "This is testing",
-    resave : false,
-    saveUninitialized : false
-}));
+app.use(express.static(__dirname + "/public"));
+app.use(methodOverride("_method"));
+app.use(flash());
+// seedDB(); //seed the database
 
+// PASSPORT CONFIGURATION
+app.use(require("express-session")({
+    secret: "Once again Rusty wins cutest dog!",
+    resave: false,
+    saveUninitialized: false
+}));
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-
-app.get("/", function(req, res){
-    res.render("landing");
+app.use(function(req, res, next){
+   res.locals.currentUser = req.user;
+   res.locals.error = req.flash("error");
+   res.locals.success = req.flash("success");
+   next();
 });
 
-//show all campgrounds
-app.get("/campgrounds", function(req, res){
-    //Get all campgrounds from DB
-    Campground.find({}, function(err, allcampgrounds){
-        if(err){
-            console.log(err);
-        } else {
-            res.render("campgrounds/index", {campgrounds: allcampgrounds});
-        }
-    });
-});
-
-//create new campgrounds
-app.post("/campgrounds", function(req, res){
-
-    var name = req.body.name; 
-    var image = req.body.image;
-    var desc = req.body.description;
-    var newCampground = {name: name, image: image, description: desc};
-
-    //Create a new campground and save to DB
-    Campground.create(newCampground, function(err, newlyCreated){
-        if(err){
-            console.log(err);
-        } else {
-            res.redirect("/campgrounds");
-        }
-    })
-
-});
-
-//show form to create new campground
-app.get("/campgrounds/new", function(req, res){
-    res.render("campgrounds/new");
-});
+app.use("/", indexRoutes);
+app.use("/campgrounds", campgroundRoutes);
+app.use("/campgrounds/:id/comments", commentRoutes);
 
 
-//shows more info about one campground
-app.get("/campgrounds/:id", function(req, res){
-    //find campground with provided ID
-    Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground){
-        if(err){
-            console.log(err);
-        } else {
-            //render show template with that campground
-            res.render("campgrounds/show", {camp: foundCampground});
-        }
-    });
-});
-
-
-
-//form for new comment
-app.get("/campgrounds/:id/comments/new", function(req, res){
-    //find campground by id
-    Campground.findById(req.params.id, function(err, campground){
-        if(err){
-            console.log(err);
-        } else {
-            res.render("comments/new", {camp: campground});
-        }
-    });
-});
-
-
-//create new comment
-app.post("/campgrounds/:id/comments", function(req, res){
-    //lookup campground using ID
-    Campground.findById(req.params.id, function(err, campground){
-        if(err){
-            console.log(err);
-            res.redirect("/campgrounds");
-        } else {
-            //create new comment
-            Comment.create(req.body.comment, function(err, comment){
-                if(err){
-                    console.log(err);
-                } else {
-                    //connect new comment to campground
-                    campground.comments.push(comment);
-                    campground.save();
-                    res.redirect("/campgrounds/" + campground._id);
-                }
-            });
-        }
-    });
-});
-
-//AUTH ROUTES
-app.get("/register", function(req, res) {
-    res.render("register");
-});
-
-app.post("/register", function(req, res) {
-    User.register(new User({username: req.body.username}), req.body.password, function(err, user) {
-        if (err){
-            console.log(err);
-            return res.render("register");
-        } else {
-            passport.authenticate("local")(req, res, function() {
-                res.redirect("/campgrounds");
-            });
-        }
-    });
-});
-
-
-
-app.listen("3000", function(){
-    console.log("YelpCamp running on port 3000");
+app.listen(3000, function(){
+   console.log("The YelpCamp Server Has Started!");
 });
--- models/campground.js
+++ models/campground.js
@@ -1,15 +1,23 @@
 var mongoose = require("mongoose");
 
-var campGroundSchema = new mongoose.Schema({
-    name : String,
-    image : String,
-    description : String,
-    comments : [
-    {
-    	type: mongoose.Schema.Types.ObjectId,
-    	ref : "Comment"
-    }
-    ]
+var campgroundSchema = new mongoose.Schema({
+   name: String,
+   price: String,
+   image: String,
+   description: String,
+   author: {
+      id: {
+         type: mongoose.Schema.Types.ObjectId,
+         ref: "User"
+      },
+      username: String
+   },
+   comments: [
+      {
+         type: mongoose.Schema.Types.ObjectId,
+         ref: "Comment"
+      }
+   ]
 });
 
-module.exports = mongoose.model("Campground", campGroundSchema);
+module.exports = mongoose.model("Campground", campgroundSchema);
