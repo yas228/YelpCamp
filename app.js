3dd60004185908d6a3d2eeef92d8ec5e82dcb018
Yawo Siatitse <asiatits@u.rochester.edu>
2019-03-12 03:17:08 -0400

    fixed lock file

--- app.js
+++ app.js
@@ -58,6 +58,6 @@ app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
 
-app.listen( process.env.PORT || 3000, function(){
+app.listen( process.env.PORT || 3000, process.env.IP, function(){
    console.log("The YelpCamp Server Has Started!");
 });
